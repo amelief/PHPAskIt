af99d127cd041136ac265855448796ea53d23d0a
amelief <evolatable@gmail.com>
2011-02-05 23:21:00 +0000

    Year update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 /*
 ===============================================================================================
-Askably 3.1 © 2005-2010 Amelie F.
+Askably 3.1 © 2005-2011 Amelie F.
 
 You may:
 - Use and edit/modify Askably however you like
